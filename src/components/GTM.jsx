376d2e357bd60b46f2669f1d2473430284aa383e
iagodt <67300318+iagodt@users.noreply.github.com>
2025-02-03 21:53:04 -0300

    Update GTM.jsx

--- src/components/GTM.jsx
+++ src/components/GTM.jsx
@@ -1,34 +1,36 @@
 "use client";
 
 import { useEffect } from "react";
-import Script from "next/script";
+import Head from "next/head";
 
-const GTM_ID = "AW-16850078520"; // Substitua pelo seu ID do GTM
+const GTM_ID = "AW-16850078520"; // Substitua pelo seu ID
 
 export default function GoogleTagManager() {
   useEffect(() => {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      window.dataLayer.push(arguments);
+    if (typeof window !== "undefined") {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", GTM_ID);
     }
-    gtag("js", new Date());
-    gtag("config", GTM_ID);
   }, []);
 
   return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+    <Head>
+      {/* Script do GTM no <head> */}
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`} />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GTM_ID}');
+          `,
+        }}
       />
-      <Script id="gtag-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GTM_ID}');
-        `}
-      </Script>
-      </>
+    </Head>
   );
 }
